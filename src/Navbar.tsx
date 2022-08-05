17f597e27d902c311a65346cf7aa106f7c8085ff
Alexander Zeitlhofer <alexander.zeitlhofer@sz-ybbs.ac.at>
2022-08-05 21:49:53 +0200

    -still fixing the ui, but making progress

--- src/Navbar.tsx
+++ src/Navbar.tsx
@@ -3,18 +3,20 @@ import './Navbar.css'
 
 function Navbar() {
   return (
-    <>
-      <div className="navbar">
-        <button className="glow-on-hover">Home</button>
-        <button className="glow-on-hover">Projekte</button>
-        <button className="glow-on-hover">Kontakt</button>
-        <button className="glow-on-hover">About</button>
-      </div>
-      <div className="nav-bar-spacer-container">
+
+      <div className="nav-container">
+        
+        <div className="navbar">
+          <button className="glow-on-hover">Home</button>
+          <button className="glow-on-hover">Projekte</button>
+          <button className="glow-on-hover">Kontakt</button>
+          <button className="glow-on-hover">About</button>
+        </div>
+
         <div className="nav-bar-spacer"/>
+
       </div>
-      
-    </>
+
   )
 }
 export default Navbar; 
